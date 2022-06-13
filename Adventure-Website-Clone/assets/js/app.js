c9837a8a7ad955accd5464271f2b48e42d641b69
Yelihi <Yelihi19@gmail.com>
2022-06-13 16:18:54 +0900

    Add header

--- Adventure-Website-Clone/assets/js/app.js
+++ Adventure-Website-Clone/assets/js/app.js
@@ -1,8 +1,19 @@
 //! === ADD HEADER STYLES ON SCROLL ===
 
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
+const openMenuIcon = document.querySelector('#menu-toggle-open');
+const closeMenuIcon = document.querySelector('#menu-toggle-close');
+const mobileMenu = document.querySelector('#mobile-menu');
 
+openMenuIcon.addEventListener('click', () => {
+    mobileMenu.classList.add('menu-opened');
+});
+
+closeMenuIcon.addEventListener('click', () => {
+    mobileMenu.classList.remove('menu-opened');
+})
 // --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
+const headerOverlay = document.querySelector('')
 
 //! === SWIPER ===
 
